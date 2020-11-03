54189b0c27dd559a046364cf83a96efd9431b9ec
iris-wang-22 <wzy_62250@hotmail.com>
2020-11-03 21:10:23 +1100

    error

--- Assignment_2/app/src/main/java/com/example/assignment_2/Util/ToastUtil.java
+++ Assignment_2/app/src/main/java/com/example/assignment_2/Util/ToastUtil.java
@@ -7,7 +7,7 @@ public class ToastUtil {
     public static Toast mToast;
     public static void showMsg(Context context, String msg){
         if (mToast == null){
-            mToast = Toast.makeText(context, msg, Toast.LENGTH_LONG);
+            mToast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
         } else{
             mToast.setText(msg);
         }
