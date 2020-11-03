9fd5420c265c319ee9f50df8becc6e46e98dbfd5
iris-wang-22 <wzy_62250@hotmail.com>
2020-11-03 17:10:59 +1100

    。

--- Assignment_2/app/src/main/java/com/example/assignment_2/Personal/NewProfileActivity.java
+++ Assignment_2/app/src/main/java/com/example/assignment_2/Personal/NewProfileActivity.java
@@ -177,9 +177,9 @@ public class NewProfileActivity extends AppCompatActivity {
     private void database() {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        String age_path = "users/"+username+"/profile/age";
-        String gender_path = "users/"+username+"/profile/gender";
-        String gender_avatar = "users/"+username+"/profile/avatar";
+        String age_path = "profile/"+username+"/age";
+        String gender_path = "profile/"+username+"/gender";
+        String gender_avatar = "profile/"+username+"/avatar";
         final DatabaseReference ageRef = database.getReference(age_path);
         final DatabaseReference genderRef = database.getReference(gender_path);
         final DatabaseReference avatarRef = database.getReference(gender_avatar);
@@ -208,9 +208,9 @@ public class NewProfileActivity extends AppCompatActivity {
                 }
 
                 Toast.makeText(NewProfileActivity.this, "Successful!", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(NewProfileActivity.this, PersonalActivity.class);
-                intent.putExtra("username", username);
-                startActivity(intent);
+//                Intent intent = new Intent(NewProfileActivity.this, PersonalActivity.class);
+//                intent.putExtra("username", username);
+//                startActivity(intent);
                 finish();
             }
         });
@@ -344,174 +344,5 @@ public class NewProfileActivity extends AppCompatActivity {
             iv_image.setImageBitmap(image);
         }
     }
-
-//    public static Bitmap base64ToBitmap(String base64Data) {
-//        byte[] bytes = Base64.decode(base64Data, Base64.URL_SAFE);
-//        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-//    }
-//
-//    private void saveImage(Bitmap bitmap) {
-//        File filesDir;
-//        if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-//            //road：storage/sdcard/Android/data/package/files
-//            filesDir = this.getExternalFilesDir("");
-//        } else {
-//            //road：data/data/package/files
-//            filesDir = this.getFilesDir();
-//        }
-//        //System.out.println("--------------------/n"+filesDir);
-//
-//        FileOutputStream fos = null;
-//        try {
-//            File file = new File(filesDir + "/icon.png");
-//
-//            long size = file.length();
-//            fos = new FileOutputStream(file);
-//            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
-//            fos.flush();
-//            fos.close();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } finally {
-//            if (fos != null) {
-//                try {
-//                    fos.flush();
-//                    fos.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
-//    }
-
-//
-//    //whether the image is local
-//    private boolean readImage() {
-//        File filesDir;
-//        if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-//            //storage/sdcard/Android/data/package/files
-//            filesDir = getExternalFilesDir("");
-//        }else{
-//            //data/data/package/files
-//            filesDir = getFilesDir();
-//        }
-//        File file = new File(filesDir,"icon.png");
-//        if(file.exists()){
-//            //storage--->ram
-//            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
-//            iv_image.setImageBitmap(bitmap);
-//            return true;
-//        }
-//        return false;
-//    }
-//    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-//    private String getPath(Uri uri) {
-//        int sdkVersion = Build.VERSION.SDK_INT;
-//        //higher version of 4.4.2
-//        if (sdkVersion >= 19) {
-//            Log.e("TAG", "uri auth: " + uri.getAuthority());
-//            if (isExternalStorageDocument(uri)) {
-//                String docId = DocumentsContract.getDocumentId(uri);
-//                String[] split = docId.split(":");
-//                String type = split[0];
-//                if ("primary".equalsIgnoreCase(type)) {
-//                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-//                }
-//            } else if (isDownloadsDocument(uri)) {
-//                final String id = DocumentsContract.getDocumentId(uri);
-//                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),
-//                        Long.valueOf(id));
-//                return getDataColumn(this, contentUri, null, null);
-//            } else if (isMediaDocument(uri)) {
-//                final String docId = DocumentsContract.getDocumentId(uri);
-//                final String[] split = docId.split(":");
-//                final String type = split[0];
-//                Uri contentUri = null;
-//                if ("image".equals(type)) {
-//                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-//                } else if ("video".equals(type)) {
-//                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-//                } else if ("audio".equals(type)) {
-//                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-//                }
-//                final String selection = "_id=?";
-//                final String[] selectionArgs = new String[]{split[1]};
-//                return getDataColumn(this, contentUri, selection, selectionArgs);
-//            } else if (isMedia(uri)) {
-//                String[] proj = {MediaStore.Images.Media.DATA};
-//                Cursor actualimagecursor = this.managedQuery(uri, proj, null, null, null);
-//                int actual_image_column_index = actualimagecursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//                actualimagecursor.moveToFirst();
-//                return actualimagecursor.getString(actual_image_column_index);
-//            }
-//        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
-//            // Return the remote address
-//            if (isGooglePhotosUri(uri))
-//                return uri.getLastPathSegment();
-//            return getDataColumn(this, uri, null, null);
-//        }
-//        // File
-//        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-//            return uri.getPath();
-//        }
-//        return null;
-//    }
-//
-//    /**
-//     * uri路径查询字段
-//     *
-//     * @param context
-//     * @param uri
-//     * @param selection
-//     * @param selectionArgs
-//     * @return
-//     */
-//    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-//        Cursor cursor = null;
-//        final String column = "_data";
-//        final String[] projection = {column};
-//        try {
-//            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-//            if (cursor != null && cursor.moveToFirst()) {
-//                final int index = cursor.getColumnIndexOrThrow(column);
-//                return cursor.getString(index);
-//            }
-//        } finally {
-//            if (cursor != null)
-//                cursor.close();
-//        }
-//        return null;
-//    }
-//    private boolean isExternalStorageDocument(Uri uri) {
-//        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-//    }
-//    public static boolean isDownloadsDocument(Uri uri) {
-//        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-//    }
-//    public static boolean isMediaDocument(Uri uri) {
-//        return "com.android.providers.media.documents".equals(uri.getAuthority());
-//    }
-//    public static boolean isMedia(Uri uri) {
-//        return "media".equals(uri.getAuthority());
-//    }
-//    /**
-//     * @param uri The Uri to check.
-//     * @return Whether the Uri authority is Google Photos.
-//     */
-//    public static boolean isGooglePhotosUri(Uri uri) {
-//        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
-//    }
-//    /**
-//     * 判断本地是否有该图片,没有则去联网请求
-//     * */
-//    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        if(readImage()){
-//            return;
-//        }
-//    }
 
 }
